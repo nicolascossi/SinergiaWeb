9e5e4e3ea394ee5057288f16791956bb0dc56c97
Nicolas <nicolasmartincossi@gmail.com>
2023-10-03 22:01:55 -0300

    first commit

---
+++ js/brochure.js
@@ -0,0 +1,4 @@
+document.getElementById('descargar-original').addEventListener('click', function() {
+    // Simular el clic en el enlace oculto para iniciar la descarga
+    document.getElementById('enlace-pdf').click();
+  });
